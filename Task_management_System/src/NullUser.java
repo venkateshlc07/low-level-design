47709b22fe4e2c9f500efff8aa9904636e77a3b6
venkatesh <venkateshlc07@gmail.com>
2024-05-18 17:14:21 +0530

    Added task_management files

--- Task_management_System/src/NullUser.java
+++
@@ -1,5 +0,0 @@
-public class NullUser extends User {
-    public NullUser(String userId, String name) {
-        super(userId, name);
-    }
-}
--- Task_management_System/src/TaskStatus.java
+++
@@ -1,3 +0,0 @@
-public enum TaskStatus {
-    OPEN, IN_PROGRESS, CLOSED
-}
